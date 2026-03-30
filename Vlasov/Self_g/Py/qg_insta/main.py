da32b1c9de98ded3a0109666075d437d57602cb5
Minoshima Takashi <minoshim@jamstec.go.jp>
2026-03-30 12:48:34 +0900

    updated

--- Vlasov/Self_g/Py/qg_insta/main.py
+++ Vlasov/Self_g/Py/qg_insta/main.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Quantum Vlasov simulation
+
 # User-set parameters
 ## Spatial grid
 xoff=2
 voff=2
-xmesh=64
-vmesh=64
+xmesh=128
+vmesh=128
 nx=xmesh+2*xoff
 nv=vmesh+2*voff
 ## Domain size and CFL number
@@ -20,6 +22,9 @@ amp=0.01                        # Perturbation amplitude
 k0=2.0*np.pi/lx                 # Fundamental wavenumber
 kk=k0*2.0                       # Perturbation wavenumber
 kj=kk/0.5                       # Jeans wavenumber
+## Quantum parameters
+hbar=1e-1
+hbar2=hbar*hbar/24.0
 # End of user-set parameters
 
 # Grid width and thermal velocity
@@ -47,6 +52,15 @@ def init(x,v,f):                  # Initialize
     fv=gaussian(v,0,vs)
     fx=1.0+amp*np.cos(kk*x)
     f[:,:]=fv[:,None]*fx[None,:]
+
+def bc1d(f,xoff,dnx=0):         # Boundary condition
+    nx = len(f)
+    if dnx == 0:
+        f[:xoff] = f[nx - 2*xoff : nx - xoff]
+        f[nx - xoff:] = f[xoff : 2*xoff]
+    elif abs(dnx) == 1:
+        f[:xoff] = dnx * f[xoff : 2*xoff][::-1]
+        f[nx - xoff:] = dnx * f[nx - 2*xoff : nx - xoff][::-1]
 
 def bc2d(f,xoff,yoff,dnx=0,dny=0): # Boundary condition
     # f : shape (ny,nx)
@@ -86,7 +100,9 @@ def cslmsl(f,v,dt,dx,xoff=2):       # Conservative SL-MUSCL scheme
     slopel=mm3(f[1:-2]-f[0:-3],f[2:-1]-f[1:-2],ftl-f[1:-2])
     sloper=mm3(f[2:-1]-f[1:-2],f[3:  ]-f[2:-1],f[2:-1]-ftr)
     flux[2:-1]=0.5*((1+sgnv)*(f[1:-2]+slopel)+(1-sgnv)*(f[2:-1]-sloper))
-    f[xoff:nx-xoff]-=nu*(flux[xoff+1:nx-xoff+1]-flux[xoff:nx-xoff])
+    rhs=-nu*(flux[xoff+1:nx-xoff+1]-flux[xoff:nx-xoff])
+    f[xoff:nx-xoff]+=rhs
+    return rhs
     
 def csl3rd(f,v,dt,dx,xoff=2):       # 3rd-order conservative semi-Lagrangian scheme
     nx=len(f)
@@ -101,18 +117,78 @@ def csl3rd(f,v,dt,dx,xoff=2):       # 3rd-order conservative semi-Lagrangian sch
     ftl=c0l+nu*(-c1*0.5+c2l*nu/3.0)
     ftr=c0r+nu*(-c1*0.5+c2r*nu/3.0)
     flux[2:-1]=0.5*((1+sgnv)*ftl+(1-sgnv)*ftr)
-    f[xoff:nx-xoff]-=nu*(flux[xoff+1:nx-xoff+1]-flux[xoff:nx-xoff])    
+    rhs=-nu*(flux[xoff+1:nx-xoff+1]-flux[xoff:nx-xoff])    
+    f[xoff:nx-xoff]+=rhs
+    return rhs
 
+def dadv1d(f,v,w,dt,dx,xoff=2,alpha=1.0):
+    # Solve df/dt+v*df/dx+w*d3f/dx3=0
+    # Fully implicit scheme
+    from scipy.linalg import solve_banded
+    nx=len(f)
+    vv =v*dt/dx                  # Advection
+    avv=alpha*vv
+    dd =0.5*np.abs(vv)           # Diffusion (numerical)
+    add=alpha*dd
+    ww =w*dt/(dx*dx*dx)          # Dispersion
+    aww=alpha*ww
+    ss =0.5*np.abs(ww)          # Hyper diffusion (numerical)
+    ass=alpha*ss
+    c1=np.array([-1./12.,+2./3.,+0.0,  -2./3.,+1./12.])
+    c2=np.array([-1./6. ,+2./3.,-1.0,  +2./3., -1./6.]) # These coefficients give 3rd-order upwind scheme for advection
+    c3=np.array([+0.5   ,-1.0  ,+0.0,    +1.0,   -0.5])
+    c4=np.array([+1.0   ,-4.0  ,+6.0,    -4.0,   +1.0])
+    ab=np.zeros((5,nx)) #banded matrix
+    ab[0, 2:]=c1[0]*avv-c2[0]*add+c3[0]*aww+c4[0]*ass #i+2
+    ab[1, 1:]=c1[1]*avv-c2[1]*add+c3[1]*aww+c4[1]*ass #i+1
+    ab[2,  :]=1.0      -c2[2]*add          +c4[2]*ass #i
+    ab[3,:-1]=c1[3]*avv-c2[3]*add+c3[3]*aww+c4[3]*ass #i-1
+    ab[4,:-2]=c1[4]*avv-c2[4]*add+c3[4]*aww+c4[4]*ass #i-2
+    rr=np.zeros(nx)     #RHS
+    rr[2:-2]=(-vv*(c1[0]*f[4:]+c1[1]*f[3:-1]+c1[2]*f[2:-2]+c1[3]*f[1:-3]+c1[4]*f[0:-4])
+              +dd*(c2[0]*f[4:]+c2[1]*f[3:-1]+c2[2]*f[2:-2]+c2[3]*f[1:-3]+c2[4]*f[0:-4])
+              -ww*(c3[0]*f[4:]+c3[1]*f[3:-1]+c3[2]*f[2:-2]+c3[3]*f[1:-3]+c3[4]*f[0:-4])
+              -ss*(c4[0]*f[4:]+c4[1]*f[3:-1]+c4[2]*f[2:-2]+c4[3]*f[1:-3]+c4[4]*f[0:-4]))
+    f[xoff:-xoff]+=solve_banded((2,2),ab[:,xoff:-xoff],rr[xoff:-xoff])
+
+def dcsl1d(f,v,w,dt,dx,xoff=2,alpha=1.0):
+    # Solve df/dt+v*df/dx+w*d3f/dx3=0
+    # CSL scheme for advection, implicit scheme for dispersion
+    from scipy.linalg import solve_banded
+    nx=len(f)
+    ww =w*dt/(dx*dx*dx)          # Dispersion
+    aww=alpha*ww
+    ss =0.5*np.abs(ww)          # Hyper diffusion (numerical)
+    ass=alpha*ss
+    c3=np.array([+0.5   ,-1.0  ,+0.0,    +1.0,   -0.5])
+    c4=np.array([+1.0   ,-4.0  ,+6.0,    -4.0,   +1.0])
+    ab=np.zeros((5,nx)) #banded matrix
+    ab[0, 2:]=c3[0]*aww+c4[0]*ass #i+2
+    ab[1, 1:]=c3[1]*aww+c4[1]*ass #i+1
+    ab[2,  :]=1.0      +c4[2]*ass #i
+    ab[3,:-1]=c3[3]*aww+c4[3]*ass #i-1
+    ab[4,:-2]=c3[4]*aww+c4[4]*ass #i-2
+    rr=np.zeros(nx)     #RHS
+    fcpy=f.copy()       # Dummy
+    rr[2:-2]=(cslmsl(fcpy,v,dt,dx,2)
+              -ww*(c3[0]*f[4:]+c3[1]*f[3:-1]+c3[2]*f[2:-2]+c3[3]*f[1:-3]+c3[4]*f[0:-4])
+              -ss*(c4[0]*f[4:]+c4[1]*f[3:-1]+c4[2]*f[2:-2]+c4[3]*f[1:-3]+c4[4]*f[0:-4]))
+    f[xoff:-xoff]+=solve_banded((2,2),ab[:,xoff:-xoff],rr[xoff:-xoff])
+    
 def pushx(f,v,dt,dx,xoff=2,voff=2):
     bc2d(f,xoff,voff,0,999) # Periodic in X, nothing to do in V
     for fi,vi in zip(f[voff:-voff,:],v[voff:-voff]):
         cslmsl(fi,vi,dt,dx,xoff)
 
-def pushv(f,g,dt,dv,xoff=2,voff=2):
+def pushv(f,g,dt,dv,dx,xoff=2,voff=2):
+    bc1d(g,xoff,0)
     nv,nx=f.shape
     for i in range(xoff,nx-xoff):
         ftmp = f[:, i].copy()
-        cslmsl(ftmp, 0.5*(g[i]+g[i+1]), dt, dv, voff)
+        d2gdx=0.5*(g[i+2]-g[i+1]-g[i]+g[i-1])/(dx*dx)
+        dcsl1d(ftmp, 0.5*(g[i]+g[i+1]), -hbar2*d2gdx, dt, dv, voff, 0.505)
+        # dadv1d(ftmp, 0.5*(g[i]+g[i+1]), -hbar2*d2gdx, dt, dv, voff, 0.505)
+        # cslmsl(ftmp, 0.5*(g[i]+g[i+1]), dt, dv, voff)
         f[voff:nv-voff, i] = ftmp[voff:nv-voff]
     
 def main(t,tmax):
@@ -133,7 +209,7 @@ def main(t,tmax):
         g[xoff:nx-xoff+1]-=gmean
         
         # Advection in V (full)
-        pushv(f,g,    dt,dv,xoff,voff)
+        pushv(f,g,    dt,dv,dx,xoff,voff)
         
         # Advection in X (half)
         pushx(f,v,0.5*dt,dx,xoff,voff)
@@ -145,3 +221,5 @@ if __name__ == "__main__":
     init(x,v,f)
     t=main(0,tmax)
     print(f"Simulation end at t = {t:.6f}")
+    plt.imshow(f[voff:-voff,xoff:-xoff],cmap='jet',origin='lower',aspect='auto',extent=[-lx/2,lx/2,-lv,lv])
+    plt.show(block=False)
